96f83b0a3bfe16cbf59cdf80477c0d26a9b7b21b
Sangam Shinde <142786495+sangampshinde@users.noreply.github.com>
2025-03-15 20:14:28 +0530

    Nested Routing

--- next-tuts/src/app/login/layout.js
+++ next-tuts/src/app/login/layout.js
@@ -1,11 +1,88 @@
+// "use client"
+// import Link from "next/link";
+// import "./logincss.css";
+
+
+
+
+// export default function Layout ({children}) {
+
+//   return (
+//     <div>
+//         <ul className="login-menu">
+//             <li>
+//                 <h4>Login Navbar</h4>
+//             </li>
+//             <li>
+//                 <Link href="/login">Main Login</Link>
+//             </li>
+//             <li>
+//                 <Link href="/login/loginstudent">Login Student</Link>
+//             </li>
+//             <li>
+//                 <Link href="/login/loginteacher">Login Teacher</Link>
+//             </li>
+//         </ul>
+//         {children}
+//     </div>
+//   )
+// }
+
+// ---------------------------------------------------------------------------
+
+// "use client"
+// import Link from "next/link";
+// import "./logincss.css";
+// import { usePathname } from "next/navigation";
+
+
+
+// export default function Layout ({children}) {
+
+//     const path = usePathname()
+//     // console.log(path)
+//   return (
+
+//     <div>
+//         { path!=="/login/loginteacher" ?
+//         (<ul className="login-menu">
+//             <li>
+//                 <h4>Login Navbar</h4>
+//             </li>
+//             <li>
+//                 <Link href="/login">Main Login</Link>
+//             </li>
+//             <li>
+//                 <Link href="/login/loginstudent">Login Student</Link>
+//             </li>
+//             <li>
+//                 <Link href="/login/loginteacher">Login Teacher</Link>
+//             </li>
+//         </ul>):null }
+        
+//         {children}
+//     </div>
+//   )
+// }
+
+// ------------------------------------------------------------------------
+
+"use client"
 import Link from "next/link";
 import "./logincss.css";
+import { usePathname } from "next/navigation";
+
 
 
 export default function Layout ({children}) {
+
+    const path = usePathname()
+    // console.log(path)
   return (
+
     <div>
-        <ul className="login-menu">
+        { path!=="/login/loginteacher" ?
+        (<ul className="login-menu">
             <li>
                 <h4>Login Navbar</h4>
             </li>
@@ -18,7 +95,8 @@ export default function Layout ({children}) {
             <li>
                 <Link href="/login/loginteacher">Login Teacher</Link>
             </li>
-        </ul>
+        </ul>):<Link href="/login">Main Login</Link> }
+        
         {children}
     </div>
   )
