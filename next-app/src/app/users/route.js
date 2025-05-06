57ede19c9863b42e5d680adeb476da5c0f60b83c
unknown <sangampshinde@gmail.com>
2025-05-06 09:18:20 -0700

    postapi

--- next-app/src/app/users/route.js
+++ next-app/src/app/users/route.js
@@ -2,8 +2,14 @@ import { NextResponse } from 'next/server'
 import { users } from '../utils/db'
 
 export async function GET(){
-
     const data= users;
     return NextResponse.json(data,{ status:200 });
+    
+}
+
+export async function POST(){
+    return NextResponse.json({
+        result:'hello'
+    })
     
 }
